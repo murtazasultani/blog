7d1c91a2de477d23d7e6cba74f28bb18ef9d800d
Murtaza Sultani <31856440+murtazasultani@users.noreply.github.com>
2022-01-09 16:58:48 +0300

    Add Meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,15 +2,30 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width">
+        <meta name="author" content="Murtaza Sultani :: https://murtazasultani.me/" />
+        <title inertia>{{ config('app.name') }}</title>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="description" content="A simple blog post app" />
+        <meta property="og:url" content="{{ config('app.url') }}">
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="{{ config('app.name') }}" />
+        <meta property="og:description" content="A simple blog post app" />
+        <meta property="og:image" content="">
+        <meta property="og:title" content="{{ config('app.name') }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@murtazasultanii"/>
+        <meta name="twitter:title" content="{{ config('app.name') }}"/>
+        <meta name="twitter:description" content="A simple blog post app"/>
+        <meta name="twitter:image" content=""/>
+        <meta name="twitter:creator" content="@murtazasultani"/>
+        <meta name="next-head-count" content="15">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/bundle.css') }}" >
+        <link rel="preload" as="font" href="{{ asset('/fonts/TiemposHeadline-Medium.woff') }}" type="font/woff" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="{{ mix('css/bundle.css') }}" />
 
         <!-- Scripts -->
         @routes
