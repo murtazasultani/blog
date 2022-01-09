8f017f30749827bb6d4c731cce73be876e23a8aa
Murtaza Sultani <31856440+murtazasultani@users.noreply.github.com>
2022-01-09 16:26:17 +0300

    Add Post Comment Migration

---
+++ database/migrations/2022_01_09_131930_create_post_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->text('comment');
+            $table->foreignId('post_id')->index();
+            $table->foreignId('comment_id')->index();
+
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('comment_id')->references('id')->on('post_comments');
+            $table->foreignId('user_id')->nullable()->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_comments');
+    }
+}
