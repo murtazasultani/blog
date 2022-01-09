2aa09eeaeb31eea5d8fa4ace5ad9c6853816172d
Murtaza Sultani <31856440+murtazasultani@users.noreply.github.com>
2022-01-09 16:46:31 +0300

    Add Eloquent Relation ship of Post and Post comment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = ['comments'];
+
+    /**
+     * Get the postComments.
+     */
+    public function comments()
+    {
+        return $this->hasMany(PostComment::class, 'post_id');
+    }
 }
--- app/Models/PostComment.php
+++ app/Models/PostComment.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class PostComment extends Model
 {
     use HasFactory;
+
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = ['comments'];
+
+    /**
+     * The post of Comment.
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    /**
+     * Get the postComments.
+     */
+    public function comments()
+    {
+        return $this->hasMany(PostComment::class, 'comment_id');
+    }
 }
