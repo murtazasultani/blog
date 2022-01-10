6c71428191b8bdf8a555976423a024cb8aef75ea
Murtaza Sultani <31856440+murtazasultani@users.noreply.github.com>
2022-01-10 20:36:39 +0300

    Add Comment Resource

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                    => $this->id,
+            'name'                  => $this->name,
+            'profile_photo_path'    => 'https://ui-avatars.com/api/?name='.urlencode($this->name).'&color=7F9CF5&background=EBF4FF',
+            'email'                 => $this->email,
+            'comment'               => $this->comment,
+            'created_at'            => $this->created_at->format('F jS, Y - H:i')
+        ];
+    }
+}
